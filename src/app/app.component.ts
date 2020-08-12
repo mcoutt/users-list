46b68258e562828e2dd99ae2071f647767c03ac7
Mykhailo_Koshelnyk <Mykhailo_Koshelnyk@epam.com>
2020-08-12 20:03:55 +0300

    init app, add material ui, add simple list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-list';
+  typesOfShoes: string[] = ['test user', 'Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
